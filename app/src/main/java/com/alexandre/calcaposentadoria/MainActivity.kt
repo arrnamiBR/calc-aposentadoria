e3403a34b9a69217c3f60bdab5427df72b934e82
20231tiimi0057 <alexandre.guerra765@gmail.com>
2025-05-08 12:11:27 -0300

    Melhora de design

--- app/src/main/java/com/alexandre/calcaposentadoria/MainActivity.kt
+++ app/src/main/java/com/alexandre/calcaposentadoria/MainActivity.kt
@@ -8,8 +8,7 @@ import com.alexandre.calcaposentadoria.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var masc:Boolean = false
-    private var fem:Boolean = false
+    private var sexo:Char = 'a'
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,31 +18,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnmasc.setOnClickListener {
-            binding.btnmasc.setBackgroundColor(ContextCompat.getColor(this, R.color.hardblue))
-            binding.btnfem.setBackgroundColor(ContextCompat.getColor(this, R.color.pink))
-            masc = true
-            fem = false
+            binding.btnmasc.setBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+            binding.btnfem.setBackgroundColor(ContextCompat.getColor(this, R.color.gray))
+            sexo = 'm'
         }
 
         binding.btnfem.setOnClickListener {
-            binding.btnmasc.setBackgroundColor(ContextCompat.getColor(this, R.color.blue))
-            binding.btnfem.setBackgroundColor(ContextCompat.getColor(this, R.color.hardpink))
-            masc = false
-            fem = true
+            binding.btnmasc.setBackgroundColor(ContextCompat.getColor(this, R.color.gray))
+            binding.btnfem.setBackgroundColor(ContextCompat.getColor(this, R.color.pink))
+            sexo = 'f'
         }
 
         binding.btnmostrar.setOnClickListener {
             val idade:Int? = binding.inputidade.text.toString().toIntOrNull()
+
             if (idade != null) {
-                if (masc) {
+                if (sexo == 'm') {
                     binding.textidade.text = "Faltam ${65 - idade} anos para você se aposentar."
-                } else if (fem) {
+                } else if (sexo == 'f') {
                     binding.textidade.text = "Faltam ${62 - idade} anos para você se aposentar."
                 } else {
                     binding.textidade.text = "Selecione um sexo."
                 }
             } else {
-                binding.textidade.text = "Insira uma idada."
+                binding.textidade.text = "Insira uma idade."
             }
         }
 
